6509222acbbabc817edeefcfaf3ec32f1a5d4a82
Jeffrey Meng <jeffkmeng@gmail.com>
2019-10-15 19:33:34 -0700

    add font awesome and github icon to basic html

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,2 +1,20 @@
-// index.ts
+import { dom, library } from '@fortawesome/fontawesome-svg-core'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import '../styles/index.scss';
+import $ from "jquery";
+
+//render a subset of font awesome icons
+library.add(faGithub);
+dom.i2svg().then(() => {
+    console.debug("Font Awesome Icons Loaded");
+});
+
+$("#join-back").click(() => {
+    $("#view-join").addClass("hidden");
+    $("#view-home").removeClass("hidden");
+})
+
+$("#home-join").click(() => {
+    $("#view-home").addClass("hidden");
+    $("#view-join").removeClass("hidden");
+});
