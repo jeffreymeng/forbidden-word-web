144c437549e12705096c5affda02d1e3780cebbf
Jeffrey Meng <jeffkmeng@gmail.com>
2019-10-17 17:03:53 -0700

    add host functionality

---
+++ src/scripts/firebase.ts
@@ -0,0 +1,16 @@
+import * as firebase from "firebase/app";
+import "firebase/firestore";
+
+const config = {
+    apiKey: "AIzaSyA4MSqu0TrSwd-UeokGmeA0Ia3r6G5NHng",
+    authDomain: "forbidden-word-online.firebaseapp.com",
+    databaseURL: "https://forbidden-word-online.firebaseio.com",
+    projectId: "forbidden-word-online",
+    storageBucket: "forbidden-word-online.appspot.com",
+    messagingSenderId: "425960262979",
+    appId: "1:425960262979:web:078ffd38fb72fce94ab3f6",
+    measurementId: "G-FT3CZPHEM1"
+};
+firebase.initializeApp(config);
+
+export default firebase;
--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -3,6 +3,8 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import '../styles/index.scss';
 import $ from "jquery";
+import firebase from "./firebase";
+
 
 //render a subset of font awesome icons
 library.add(faGithub, faCircle);
@@ -14,14 +16,52 @@ async function validateJoinCode() {
     });
 }
 
+async function generateRandomId(tries = 0, minLength = 5) {
+    console.log(tries, minLength);
+    if (tries > 100) {
+
+        minLength += 2;
+    }
+    if (tries > 1000) {
+        throw "Random Error";
+        return false;
+    }
+
+    // Avoid 0 and O because they are ambiguous.
+    let chars = "123456789ABCDEFGHIJKLMNPQRSTUVWXYZ";
+    let result = "";
+    for (let i = minLength; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+    console.log(result);
+    return firebase.firestore()
+            .collection("games")
+            .doc(result)
+            .get()
+            .then(function(doc) {
+                console.log(doc.exists);
+                if (!doc.exists) {
+                    console.log(result);
+                    return result;
+                } else {
+                    generateRandomId(tries + 1, minLength)
+                        .then(function (id) {
+                            console.log("TRY AGAIN");
+                            return id;
+                        })
+                }
+            })
+}
+
 $("#home-host").click(() => {
     $(".view").addClass("hidden");
     $("#view-host").removeClass("hidden");
+    $("#host-name").focus();
 });
 
 $("#home-join").click(() => {
+
     $(".view").addClass("hidden");
     $("#view-join").removeClass("hidden");
+    $("#join-code").focus();
 });
 
 $("#home-rules").click(() => {
@@ -42,4 +82,31 @@ $("#join-btn").click(function() {
         }
     });
 
+});
+
+$("#host-btn").click(function() {
+
+    generateRandomId().then(function(id) {
+        if (id === false) {
+            alert("error");
+            return;
+
+        }
+        console.log(id);
+        firebase.firestore()
+                .collection("games")
+                .doc(id)
+                .set({
+                    timestamp:new Date().getTime(),
+                    active:true,
+                    users:[
+                        $("#host-name").val()
+                    ]
+                }).then(function() {
+                    console.log("DONE");
+                }).catch(function(e) {
+                    console.log("ERROR", e);
+                });
+
+    });
 });
